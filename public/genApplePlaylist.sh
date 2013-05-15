c6ee0dbc1dd125f2942b9ec4c15ad5f2c03591ef
Mark Essel <messel@gmail.com>
2013-05-15 12:00:01 -0400

    apple toolchain generated separate audio and video streams are hi speed scrubbable

--- public/genApplePlaylist.sh
+++ public/genApplePlaylist.sh
@@ -1,3 +1,6 @@
 # if you have os x, worth comparing
-mediafilesegmenter -t 10 --generate-variant-plist -z iframe_index.m3u8 mainWithAudio.mp4
-variantplaylistcreator prog_index.m3u8  mainWithAudio.plist -i iframe_index.m3u8
+mediafilesegmenter -t 10 -i video_index.m3u8 --generate-variant-plist -B videoOnly -A -z iframe_index.m3u8 mainWithAudio.mp4
+mv mainWithAudio.plist videoOnly.plist
+mediafilesegmenter -t 10 -i audio_index.m3u8 --generate-variant-plist -B audioOnly -a mainWithAudio.mp4
+mv mainWithAudio.plist audioOnly.plist
+variantplaylistcreator -o junk.m3u8 video_index.m3u8  videoOnly.plist -i iframe_index.m3u8 audio_index.m3u8 audioOnly.plist
--- public/genMainWithAudio.sh
+++ public/genMainWithAudio.sh
@@ -1,3 +1,4 @@
 ./messWithAudioStream.sh
-ffmpeg -i main.ts -i audio.aac -qscale 0 -vcodec copy -acodec copy -absf aac_adtstoasc -threads 0 -y mainWithAudio.mp4
+# ffmpeg -i main.ts -i audio.aac -qscale 0 -vcodec copy -acodec copy -absf aac_adtstoasc -threads 0 -y mainWithAudio.mp4
+ffmpeg -i main.ts -i audio.wav -qscale 0 -vcodec copy -acodec libfaac -ab 128k -ar 48000 -threads 0 -y mainWithAudio.mp4
 ffmpeg -i mainWithAudio.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb mainWithAudio.ts
